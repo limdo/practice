0bb4b8b9d788910a6dbe873e6599a4a3de3ab555
Leemdo <ljh970199@naver.com>
2020-04-14 01:24:11 +0900

    댓글 관련 기능 추가, todo: comment 수정/제거 기능 추가하기

---
+++ bootstrap-bbs/src/components/CommentList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div :key="item.commnent_id" v-for="item in comments">
+            <CommentListItem :commentObj="item"/>
+        </div>
+        <CommentCreate :contentId="contentId" :reloadComments="reloadComments"/>
+    </div>
+</template>
+
+<script>
+import data from '@/data'
+import CommentListItem from './CommentListItem';
+import CommentCreate from './CommentCreate';
+
+export default {
+    name: "CommentList",
+    props: {
+        contentId: Number
+    },
+    components: {
+        CommentListItem,
+        CommentCreate,
+    },
+    data() {
+        return {
+            comments: data.Comment.filter(item => item.content_id === this.contentId),
+        }
+    },
+    methods: {
+        reloadComments() {
+            this.comments = data.Comment.filter(item => item.content_id === this.contentId)
+        }
+    }
+}
+</script>
