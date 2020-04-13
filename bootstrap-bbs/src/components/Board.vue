4bda059b0a8ebfa336c6e87f0e638d34993adfed
Leemdo <ljh970199@naver.com>
2020-04-14 00:36:01 +0900

    ContentDetail, Create 구현, Board 수정

--- bootstrap-bbs/src/components/Board.vue
+++ bootstrap-bbs/src/components/Board.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
-    <b-table striped hover :items="items" :fields="fields"></b-table>
+    <b-table striped hover :items="items" :fields="fields" @row-clicked="rowClick"></b-table>
+    <b-button @click="writeContent">글쓰기</b-button>
   </div>
 </template>
 
 <script>
-import data from '@/data'
+import data from "@/data";
 
 export default {
+    name: 'Board',
     data() {
+        let items = data.Content.sort((a, b) => {return b.content_id - a.content_id})
+        items = items.map(contentItem => {return {...contentItem, user_name: data.User.filter(userItem => userItem.user_id === contentItem.user_id)[0].name}})
+
         return {
             fields: [
                 {
@@ -22,10 +27,26 @@ export default {
                 {
                     key: 'created_at',
                     label: '작성일'
-                }
+                },
+                {
+                    key: 'user_name',
+                    label: '글쓴이'
+                },
             ],
-            items: data.Content
-        };
+            items: items
+        }
+    },
+    methods: {
+        rowClick(item, index, e) {
+            this.$router.push({
+                path: `/board/free/detail/${item.content_id}`
+            })
+        },
+        writeContent() {
+            this.$router.push({
+                path: '/board/free/create'
+            })
+        }
     }
-};
+}
 </script>
