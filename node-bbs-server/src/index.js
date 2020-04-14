3c029a2092ad81a9d851245a18944ed9a73d2928
Leemdo <ljh970199@naver.com>
2020-04-14 22:44:06 +0900

    koa test

--- node-bbs-server/src/index.js
+++ node-bbs-server/src/index.js
@@ -1,8 +1,21 @@
 const Koa = require('koa');
 const app = new Koa();
 
-app.use(async ctx => {
-  ctx.body = 'Hello World hihihi';
+app.use(async (ctx, next) => {
+    console.log('Test')
+    ctx.set("Limdo", 'LDH');
+    await next();
+    console.log('Finished.')
+});
+
+app.use(async (ctx, next) => {
+    console.log('1. asdf')
+    await next();
+});
+
+app.use(async (ctx, next) => {
+    console.log('2. asdf')
+    ctx.body = 'TEST'
 });
 
 app.listen(3000);
