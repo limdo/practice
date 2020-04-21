d274cfb7c2ac4204722336343d70b9d400bb371d
Leemdo <ljh970199@naver.com>
2020-04-21 19:35:53 +0900

    db 테스트

--- node-bbs-server/src/database/index.js
+++ node-bbs-server/src/database/index.js
@@ -3,13 +3,11 @@ require('dotenv').config();
 var db = require('knex')({
     client: 'mysql',
     connection: {
-        host : '127.0.0.1',
-        user : 'limdo',
+        host : process.env.DB_HOST,
+        user : process.env.DB_USER,
         password : process.env.DB_PASSWORD,
         database : 'nodeapptest',
     }
 });
-
-const ret = db.raw('select now()').then((item) => { console.log(item[0])})
 
 module.exports = db
